6549516a961bb64d7be4513e184e3655655753c4
421538074 <421538074@qq.com>
2019-06-04 15:13:10 +0800

    分销模块添加退货流程

--- js/agent-service.js
+++ js/agent-service.js
@@ -4,13 +4,34 @@ var xm = new Vue({
         isshade: false,
         isrefund: false,
         isreply: true,
+        hide: false,
         cur: 0,
         arr: [{}, {}, {}],
     },
     methods: {
+        tab(t) {
+            this.cur = t
+            if (t == 0) {
+                this.hide = false
+            }else {
+                this.hide = true
+            }
+        },
         serviceDetail() {
             window.location.href = "agent-service-detail.html"
         },
+        noAgree() {
+            this.isrefund = true
+        },
+        agree() {
+            this.isshade = true
+        },
+        close() {
+            this.isshade = false
+        },
+        openReply() {
+            this.isreply = !this.isreply
+        }
     },
 
 });
